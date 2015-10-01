3943a12384ab6c77ca29b346ca158c565ac61216
Jon Ewalt & Sara Merriman <student@epicodus.com>
2015-09-30 20:10:13 -0700

    manipulate the dom

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,20 @@ $(document).ready(function() {
   $("#external-click").click(function() {
     $("#external").slideToggle();
   });
+
+  $("button#hello").click(function() {
+    $("ul#user").prepend ("<li>hi</li>");
+    $("li").click(function() {
+      $(this).remove();
+    });
+  });
+
+  $("button#goodbye").click(function() {
+    $("ul#webpage").prepend ("<li>hi</li>");
+    $("li").click(function() {
+      $(this).remove();
+    });
+  });
+
+
 });
