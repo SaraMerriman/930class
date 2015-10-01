d539d397cb327a8535bb5a3fb8ace3eb3e0c92c0
Jon Ewalt & Sara Merriman <student@epicodus.com>
2015-09-30 19:40:18 -0700

    Finished initial contractor site

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $("#SOW-click").click(function() {
     $("#SOW").slideToggle();
   });
+
+  $("#external-click").click(function() {
+    $("#external").slideToggle();
+  });
 });
